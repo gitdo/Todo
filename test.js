5a30d09aa5e20e412903bc11f5345a2edcd5f2bf
gitdo <gitdo@mailinator.com>
2013-11-09 09:47:44 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,8 @@
-
 /* This is a test JS file
 // @todo: something */
 
-var x = "I'm a president"
+var x = "I'm a president" // True story
+
+function todo() {
+  console.log(x); // @todo: Play win big big 
+}

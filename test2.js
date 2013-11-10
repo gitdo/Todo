ae179a278b40d7677901445eb1657d37cf43f652
gitdo <gitdo@users.noreply.github.com>
2013-11-10 06:52:09 -0700

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,14 +1,2 @@
-/* TEST JS
-@todo: do something */
-
-// @todo: one more thing
-
-/**
- * @TODO:
- * Need to seriously make an awesome js file now...
- * /
-
-
-
-/* @todo: finish something else */  
+// Only one to do
  
